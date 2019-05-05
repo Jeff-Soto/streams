0431935c2ffaeb719351275947bc67c66ccd6a6a
Aly Soto <jsotodevelopment@gmail.com>
2019-05-04 22:10:36 -0400

    Add Header component and fix file tyop

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 // import { BrowserRouter, Route, Link } from 'react-router-dom';
-import StreamList from './streams/StreamList'
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <StreamList />
     </div>
   );
